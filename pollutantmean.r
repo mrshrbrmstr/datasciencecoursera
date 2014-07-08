c5858d24478149c97cb6ea18502209c17f9bd44f
mrshrbrmstr <Mary@Marys-MacBook-Pro-2.local>
2014-07-08 16:53:04 -0400

    Add line to prevent division by 0

--- pollutantmean.r
+++ pollutantmean.r
@@ -9,6 +9,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
                   Sum.pollutant <- Sum.pollutant + sum(tmp$nitrate, na.rm = false)
                   Count.pollutant <- Count.pollutant + sum(!is.na(tmp$nitrate))
              }
+             Count.pollutant <- max(1, Count.pollutant)  ## Prevents division by 0
              return(Sum.pollutant/Count.pollutant)
      }
      if (pollutant == "sulfate") {
@@ -18,6 +19,7 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
                   Sum.pollutant <- Sum.pollutant + sum(tmp$sulfate, na.rm = false)
                   Count.pollutant <- Count.pollutant + sum(!is.na(tmp$sulfate))
              }
+             Count.pollutant <- max(1, Count.pollutant)  ## Prevents division by 0
              return(Sum.pollutant/Count.pollutant)
      }
      if (pollutant != "nitrate" & pollutant != "sulfate") {
