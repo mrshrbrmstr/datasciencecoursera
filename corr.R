9bd3940b02c43062f91dd0c40add8af0958dba9c
mrshrbrmstr <Mary@Marys-MacBook-Pro-2.local>
2014-07-10 22:24:38 -0400

    Revision 2
    Read data files once only
    extract only rows that satisfy the threshold

--- corr.R
+++ corr.R
@@ -1,21 +1,23 @@
 corr <- function(directory, threshold = 0) {
   
-  cur <- getwd()                                           ## Store current working directory
+  cur <- getwd()                                      ## Store current working directory
+  
   size.corr <- 0
-  corr.vector <- 0
-  for (i in 1:332) {
+  corr.vector <- numeric(0)                           ## Creates an empty numeric vector of length 0
+  
+  cases <- complete(directory, 1:332)                 ## this gets all the complete cases
+  ids <- cases[cases$nobs > threshold,]$id            ## cases[cases$nobs > threshold] is a sub-table with only the rows
+          ## that satisfy the logical statement "cases$nobs > threshold" $id selects the id column vector
+  for (i in ids) {                                    ## this iterates over that ids vector
+    
     setwd(directory)                                  ## Set wd to correct file location
     Char.RecordNumber <- sprintf("%03d.csv", i)       ## Converts numeric id into a string of chr
     tmp <- read.csv(Char.RecordNumber)                ## Reads the file and stores in tmp
     setwd(cur)                                        ## Restores wd prior to other function
-    nobs <- complete(directory, i)
-     if (nobs[1] > threshold) {
-      
-      size.corr <- size.corr + 1
-      corr.vector[size.corr] <- round(cor(tmp$sulfate, tmp$nitrate, use = "na.or.complete", method = "pearson"), digits = 5)
-      
-    }
-
+    
+    size.corr <- size.corr + 1
+    corr.vector[size.corr] <- round(cor(tmp$sulfate, tmp$nitrate, use = "pairwise.complete.obs"), digits=5)
+    
   }
   
   return(corr.vector)
